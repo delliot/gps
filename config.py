a3925df7b1615e7ab4a4fe9e8d9298b672ebd4b4
Delan Elliot <delan.elliot@gmail.com>
2017-11-08 03:08:14 -0800

    comments

--- config.py
+++ config.py
@@ -1,3 +1,24 @@
+'''
+/*------------------------------------------------------------------------------------------------------------------
+-- SOURCE FILE:		config.py
+--
+-- FUNCTIONS:
+--					n/a
+--
+--
+-- DATE:			November 7, 2017
+--
+-- REVISIONS:		N/A
+--
+-- DESIGNER:		Delan Elliot
+--
+-- PROGRAMMER:		Delan Elliot, Roger Zhang
+--
+-- NOTES:
+-- holds globally available variables
+----------------------------------------------------------------------------------------------------------------------*/
+'''
+
 gpsd = None
 textBox = None
 canvas = None
